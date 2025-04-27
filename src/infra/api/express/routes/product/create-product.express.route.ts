d4fcfdcf7ad6c9287d42f16e352d666e17ab91cf
martinsllm <lucaslm2010@gmail.com>
2025-04-27 20:37:26 -0300

    Configuring the infra layer

---
+++ src/infra/api/express/routes/product/create-product.express.route.ts
@@ -0,0 +1,57 @@
+import { Request, Response } from "express";
+import {
+  CreateProductUsecase,
+  CreateProductInputDto,
+} from "../../../../../usecases/product/create-product.usecase";
+import { HttpMethod, Route } from "../routes";
+
+export type CreateProductResponseDto = {
+  id: string;
+};
+
+export class CreateProductRoute implements Route {
+  private constructor(
+    private readonly path: string,
+    private readonly method: HttpMethod,
+    private readonly createProductService: CreateProductUsecase
+  ) {}
+
+  public static create(createProductService: CreateProductUsecase) {
+    return new CreateProductRoute(
+      "/products",
+      HttpMethod.POST,
+      createProductService
+    );
+  }
+
+  public getHandler() {
+    return async (req: Request, res: Response) => {
+      const { name, price } = req.body;
+
+      const input: CreateProductInputDto = {
+        name,
+        price,
+      };
+
+      const output: CreateProductResponseDto =
+        await this.createProductService.execute(input);
+
+      const responseBody = this.present(output);
+
+      res.status(201).json(responseBody).send();
+    };
+  }
+
+  public getPath(): string {
+    return this.path;
+  }
+
+  public getMethod(): HttpMethod {
+    return this.method;
+  }
+
+  private present(input: CreateProductResponseDto): CreateProductResponseDto {
+    const response = { id: input.id };
+    return response;
+  }
+}
---
+++ src/infra/api/express/routes/product/list-product.express.route.ts
@@ -0,0 +1,61 @@
+import { Request, Response } from "express"
+import {
+    ListProductInputDto,
+    ListProductOutputDto,
+    ListProductUsecase,
+} from "../../../../../usecases/product/list-product.usecase"
+import { HttpMethod, Route } from "../routes"
+
+export type ListProductResponseDto = {
+    products: {
+        id: string
+        name: string
+        price: number
+    }[]
+}
+
+export class ListProductRoute implements Route {
+    private constructor(
+        private readonly path: string,
+        private readonly method: HttpMethod,
+        private readonly listProductService: ListProductUsecase
+    ) {}
+
+    public static create(listProductService: ListProductUsecase) {
+        return new ListProductRoute(
+            "/products",
+            HttpMethod.GET,
+            listProductService
+        )
+    }
+
+    public getHandler() {
+        return async (req: Request, res: Response) => {
+            const output = await this.listProductService.execute()
+
+            const responseBody = this.present(output)
+
+            res.status(200).json(responseBody).send()
+        }
+    }
+
+    public getPath(): string {
+        return this.path
+    }
+
+    public getMethod(): HttpMethod {
+        return this.method
+    }
+
+    private present(input: ListProductOutputDto): ListProductResponseDto {
+        const response: ListProductResponseDto = {
+            products: input.products.map((p) => ({
+                id: p.id,
+                name: p.name,
+                price: p.price,
+            })),
+        }
+
+        return response
+    }
+}
