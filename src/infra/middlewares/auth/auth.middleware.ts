13e32ac6bfade0733deb68a69bb6c6a4b12455e7
martinsllm <lucaslm2010@gmail.com>
2025-05-11 18:40:59 -0300

    Finishing user authentication

--- src/infra/middlewares/auth/auth.middleware.ts
+++ src/infra/middlewares/auth/auth.middleware.ts
@@ -1,7 +1,12 @@
 import { Request, Response, NextFunction } from "express"
-import { UnauthorizedError } from "../errors/api-errors"
+import { BadRequestError, UnauthorizedError } from "../errors/api-errors"
 import { Auth } from "./auth.interface"
 import { UserInterface } from "../../../domain/user/interface/user.interface"
+import jwt from "jsonwebtoken"
+
+type JwtPayload = {
+    id: string
+}
 
 export class AuthMiddleware implements Auth {
     private constructor(private readonly userInterface: UserInterface) {}
@@ -14,6 +19,15 @@ export class AuthMiddleware implements Auth {
         return async (req: Request, res: Response, next: NextFunction) => {
             const { authorization } = req.headers
             if (!authorization) throw new UnauthorizedError("Unauthorized!")
+
+            const decodedToken = authorization.split(" ")[1]
+            const { id } = jwt.verify(
+                decodedToken,
+                process.env.JWT_SECRET ?? ""
+            ) as JwtPayload
+
+            const user = await this.userInterface.findById(id)
+            if (!user) throw new UnauthorizedError("Unauthorized!")
 
             next()
         }
